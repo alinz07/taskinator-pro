1e3d6a033dfca7f000707e986b0577297c32b94f
Tony Linz <tonyllinz@gmail.com>
2021-12-31 16:36:01 -0600

    add datepicker and auditTask for bootstrap coloring for overdue and soon to be due tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,24 @@
 var tasks = {};
 
+var auditTask = function(taskEl) {
+  //get date from task element
+  var date = $(taskEl).find("span").text().trim();
+
+  //convert to moment object at 5pm
+  var time = moment(date, "L").set("hour", 17);
+  
+  //remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  //apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
+
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
@@ -13,6 +32,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  //check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -90,9 +111,7 @@ $(".list-group").on("blur", "textarea", function() {
 //due date was clicked
 $(".list-group").on('click', 'span', function() {
   //get current text
-  var date = $(this)
-    .text()
-    .trim();
+  var date = $(this).text().trim();
 
   //create new input element
   var dateInput = $("<input>")
@@ -103,17 +122,27 @@ $(".list-group").on('click', 'span', function() {
   //swap out elements
   $(this).replaceWith(dateInput);
 
-  //automatically focus on new element
+  //enable jquery ui datepicker;
+  dateInput.datepicker({
+    minDate: -1,
+    onClose: function() {
+      //when calendar is closed, force a "change" event on the 'dateInput'
+      $(this).trigger("change");
+    }
+  });
+
+  //automatically bring up the calendar
   dateInput.trigger("focus");
+
 })
 
-$(".list-group").on('blur', "input[type='text']", function() {
+$(".list-group").on('change', "input[type='text']", function() {
   //get current text
   var date = $(this)
   .val()
   .trim();
 
-  //get te parent ul's id attribute
+  //get the parent ul's id attribute
   var status = $(this)
     .closest(".list-group")
     .attr("id")
@@ -135,6 +164,9 @@ $(".list-group").on('blur', "input[type='text']", function() {
 
   //replace input with span element
   $(this).replaceWith(taskSpan);
+
+  //pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 
@@ -246,4 +278,7 @@ $("#trash").droppable({
   }
 })
 
+$("#modalDueDate").datepicker({
+  minDate: -1
+});
 
